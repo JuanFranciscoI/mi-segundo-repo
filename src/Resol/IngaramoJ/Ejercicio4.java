570647ddb84178d64dd836f9feb1a0499a47a57a
Juan Francisco Ingaramo <juanfra08@live.com>
2025-05-22 20:41:20 -0300

    prueba

--- src/Resol/IngaramoJ/Ejercicio4.java
+++ src/Resol/IngaramoJ/Ejercicio4.java
@@ -7,7 +7,8 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese su año de nacimiento: ");
-        int año = scanner.nextInt();
+
+        int anio = scanner.nextInt();
 
         System.out.print("Ingrese su mes de nacimiento: ");
         int mes = scanner.nextInt();
@@ -15,7 +16,7 @@ public class Ejercicio4 {
         System.out.print("Ingrese su dia de nacimiento: ");
         int dia = scanner.nextInt();
 
-        LocalDate nacimiento = LocalDate.of(año, mes, dia);
+        LocalDate nacimiento = LocalDate.of(anio, mes, dia);
 
         System.out.println("Naciste el dia " + nacimiento.getDayOfWeek());
         // similar al ejercicio 2, vemos la nueva funcion de obtener el dia de la semana (en ingles)
